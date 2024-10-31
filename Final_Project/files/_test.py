6d5d6a39db177d565afe4f68bc9b3bcb4d89e5d5
麦家俊 <im_mjj@qq.com>
2024-11-01 02:00:41 +0800

    🧩: complete on dstruct.py for 4 kinds of CNN

--- Final_Project/files/_test.py
+++ Final_Project/files/_test.py
@@ -1,12 +1,40 @@
+import os
+
 import torch.nn as nn
 from collections import OrderedDict
 from typing import OrderedDict as TypingOrderedDict
+import random
+from collections import OrderedDict
+from modulefinder import Module
+from typing import Tuple, Union, List, OrderedDict as TypingOrderedDict
 
-from Final_Project.files import utils, dstruct
+import numpy as np
+import scipy.io as sio
+from torch.utils.data import Dataset
 
-selected_seq = dstruct.candidate_seq[0]
-selected_act = dstruct.candidate_activation_func[0]
+# from Final_Project.files import utils, dstruct
+#
+# selected_seq = dstruct.candidate_seq[0]
+# selected_act = dstruct.candidate_activation_func[0]
+#
+# final_seq = utils.mix_seq_and_act(selected_seq, selected_act)
+#
+# print(final_seq)
 
-final_seq = utils.mix_seq_and_act(selected_seq, selected_act)
 
-print(final_seq)
+dir_path = os.path.exists("../data") and "../data/SVHN_mat" or "./data/SVHN_mat"
+train_path = os.path.join(dir_path, "train_32x32.mat")
+test_path = os.path.join(dir_path, "test_32x32.mat")
+
+x = sio.loadmat(train_path)['X']
+y = sio.loadmat(test_path)['X']
+
+train_data = np.transpose(x, (2, 0, 1, 3)).astype(np.float32) / 256.0
+test_data = np.transpose(y, (2, 0, 1, 3)).astype(np.float32) / 256.0
+
+both_data = np.concatenate((train_data, test_data), axis=3)
+
+BOTH_norm_mean = [np.mean(x) for x in both_data]
+BOTH_norm_std = [np.std(x, ddof=0) for x in both_data]
+
+
--- Final_Project/files/dstruct.py
+++ Final_Project/files/dstruct.py
@@ -1,10 +1,10 @@
 import random
 from collections import OrderedDict
-from modulefinder import Module
-from typing import Tuple, Union, List, OrderedDict as TypingOrderedDict
+from typing import Tuple, Union, List, OrderedDict as TypingOrderedDict, Optional
 
 import numpy as np
 import scipy.io as sio
+import torch
 import torch.nn as nn
 from torch.utils.data import Dataset
 
@@ -85,9 +85,44 @@ class AddBiasTransform:
 
 # ==================================================================== #
 
-candidate_seq: List[Tuple[Union[TypingOrderedDict[str, None], TypingOrderedDict[str, nn.Module]],
-                          Union[TypingOrderedDict[str, None], TypingOrderedDict[str, nn.Module]]]] = [
-    (OrderedDict([  # first struct
+class Inception(nn.Module):
+    def __init__(self, in_channels: int, ch1x1: int, ch3x3_reduce: int, ch3x3: int,
+                 ch5x5_reduce: int, ch5x5: int, pool_proj: int):
+        super(Inception, self).__init__()
+
+        # 1x1 conv batch
+        self.branch1x1 = nn.Conv2d(in_channels, ch1x1, kernel_size=1)
+
+        # 1x1 -> 3x3 conv batch
+        self.branch3x3 = nn.Sequential(
+            nn.Conv2d(in_channels, ch3x3_reduce, kernel_size=1),
+            nn.Conv2d(ch3x3_reduce, ch3x3, kernel_size=3, padding=1)
+        )
+
+        # 1x1 -> 5x5 conv batch
+        self.branch5x5 = nn.Sequential(
+            nn.Conv2d(in_channels, ch5x5_reduce, kernel_size=1),
+            nn.Conv2d(ch5x5_reduce, ch5x5, kernel_size=5, padding=2)
+        )
+
+        # 3x3 pool -> 1x1 conv batch
+        self.branch_pool = nn.Sequential(
+            nn.MaxPool2d(kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(in_channels, pool_proj, kernel_size=1)
+        )
+
+    def forward(self, x):
+        branch1x1 = self.branch1x1(x)
+        branch3x3 = self.branch3x3(x)
+        branch5x5 = self.branch5x5(x)
+        branch_pool = self.branch_pool(x)
+
+        outputs = torch.cat([branch1x1, branch3x3, branch5x5, branch_pool], 1)
+        return outputs
+
+
+candidate_seq: List[Tuple[TypingOrderedDict[str, Optional[nn.Module]], TypingOrderedDict[str, Optional[nn.Module]]]] = [
+    (OrderedDict([  # first struct: SmallVGG
         ('conv1', nn.Conv2d(3, 8, kernel_size=3, padding=1)),
         ('*1', None),
         ('conv2', nn.Conv2d(8, 16, kernel_size=3, padding=1)),
@@ -112,76 +147,81 @@ candidate_seq: List[Tuple[Union[TypingOrderedDict[str, None], TypingOrderedDict[
         ('*2', None),
         ('fc3', nn.Linear(256, 10))
     ])),
-    (OrderedDict([  # second struct
-        ('conv1', nn.Conv2d(3, 8, kernel_size=3, padding=1)),
+
+    (OrderedDict([  # second struct: LeNet-5
+        ('conv1', nn.Conv2d(3, 12, kernel_size=5, stride=1, padding=2)),
         ('*1', None),
-        ('conv2', nn.Conv2d(8, 16, kernel_size=3, padding=1)),
+        ('avg1', nn.AvgPool2d(kernel_size=2, stride=2)),
+
+        ('conv2', nn.Conv2d(12, 32, kernel_size=5)),
         ('*2', None),
-        ('max1', nn.MaxPool2d(kernel_size=2, stride=2)),  # 16x16
-
-        ('conv3', nn.Conv2d(16, 32, kernel_size=3, padding=1)),
-        ('*3', None),
-        ('conv4', nn.Conv2d(32, 32, kernel_size=3, padding=1)),
-        ('*4', None),
-        ('max2', nn.MaxPool2d(kernel_size=2, stride=2)),  # 8x8
-
-        ('conv5', nn.Conv2d(32, 32, kernel_size=3, padding=1)),
-        ('*5', None),
-        ('conv6', nn.Conv2d(32, 32, kernel_size=3, padding=1)),
-        ('*6', None),
-        ('max3', nn.MaxPool2d(kernel_size=2, stride=2))  # 4x4
+        ('avg2', nn.AvgPool2d(kernel_size=2, stride=2)),
     ]), OrderedDict([
-        ('fc1', nn.Linear(32 * 4 * 4, 256)),
-        ('*1', None),
-        ('fc2', nn.Linear(256, 10))
+        ('fc1', nn.Linear(32 * 6 * 6, 256)),
+        ('*3', None),
+        ('fc2', nn.Linear(256, 128)),
+        ('*4', None),
+        ('fc3', nn.Linear(128, 10))
     ])),
-    (OrderedDict([  # third struct
-        ('conv1', nn.Conv2d(3, 8, kernel_size=3, padding=1)),
+
+    (OrderedDict([  # third struct: 2012AlexNet
+        ('conv1', nn.Conv2d(3, 64, kernel_size=5, stride=1, padding=2)),  # 32x32
         ('*1', None),
-        ('conv2', nn.Conv2d(8, 16, kernel_size=3, padding=1)),
-        ('*2', None),
         ('max1', nn.MaxPool2d(kernel_size=2, stride=2)),  # 16x16
 
-        ('conv3', nn.Conv2d(16, 32, kernel_size=3, padding=1)),
-        ('*3', None),
-        ('conv4', nn.Conv2d(32, 32, kernel_size=3, padding=1)),
-        ('*4', None),
+        ('conv2', nn.Conv2d(64, 192, kernel_size=5, padding=2)),
+        ('*2', None),
         ('max2', nn.MaxPool2d(kernel_size=2, stride=2)),  # 8x8
 
-        ('conv5', nn.Conv2d(32, 32, kernel_size=3, padding=1)),
+        ('conv3', nn.Conv2d(192, 384, kernel_size=3, padding=1)),
+        ('*3', None),
+
+        ('conv4', nn.Conv2d(384, 256, kernel_size=3, padding=1)),
+        ('*4', None),
+
+        ('conv5', nn.Conv2d(256, 256, kernel_size=3, padding=1)),
         ('*5', None),
-        ('conv6', nn.Conv2d(32, 32, kernel_size=3, padding=1)),
-        ('*6', None),
         ('max3', nn.MaxPool2d(kernel_size=2, stride=2))  # 4x4
     ]), OrderedDict([
-        ('fc1', nn.Linear(32 * 4 * 4, 256)),
-        ('*1', None),
-        ('fc2', nn.Linear(256, 10))
+        ('fc1', nn.Linear(256 * 4 * 4, 4096)),  # 256 * 4 * 4 = 4096
+        ('*6', None),
+        ('dropout1', nn.Dropout()),
+
+        ('fc2', nn.Linear(4096, 4096)),
+        ('*7', None),
+        ('dropout2', nn.Dropout()),
+
+        ('fc3', nn.Linear(4096, 10))
     ])),
-    (OrderedDict([  # fourth struct
-        ('conv1', nn.Conv2d(3, 8, kernel_size=3, padding=1)),
-        ('*1', None),
-        ('conv2', nn.Conv2d(8, 16, kernel_size=3, padding=1)),
-        ('*2', None),
-        ('max1', nn.MaxPool2d(kernel_size=2, stride=2)),  # 16x16
 
-        ('conv3', nn.Conv2d(16, 32, kernel_size=3, padding=1)),
-        ('*3', None),
-        ('conv4', nn.Conv2d(32, 32, kernel_size=3, padding=1)),
-        ('*4', None),
-        ('max2', nn.MaxPool2d(kernel_size=2, stride=2)),  # 8x8
+    (OrderedDict([  # fourth struct: 2014GoogLeNet
+        ('conv1', nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3)),
+        ('max1', nn.MaxPool2d(kernel_size=3, stride=2, padding=1)),
 
-        ('conv5', nn.Conv2d(32, 32, kernel_size=3, padding=1)),
-        ('*5', None),
-        ('conv6', nn.Conv2d(32, 32, kernel_size=3, padding=1)),
-        ('*6', None),
-        ('max3', nn.MaxPool2d(kernel_size=2, stride=2))  # 4x4
+        ('conv2', nn.Conv2d(64, 64, kernel_size=1)),
+        ('conv3', nn.Conv2d(64, 192, kernel_size=3, padding=1)),
+        ('max2', nn.MaxPool2d(kernel_size=3, stride=2, padding=1)),
+
+        # Inception 模块
+        ('inception3a', Inception(192, 64, 96, 128, 16, 32, 32)),
+        ('inception3b', Inception(256, 128, 128, 192, 32, 96, 64)),
+        ('max3', nn.MaxPool2d(kernel_size=3, stride=2, padding=1)),
+
+        ('inception4a', Inception(480, 192, 96, 208, 16, 48, 64)),
+        ('inception4b', Inception(512, 160, 112, 224, 24, 64, 64)),
+        ('inception4c', Inception(512, 128, 128, 256, 24, 64, 64)),
+        ('inception4d', Inception(512, 112, 144, 288, 32, 64, 64)),
+        ('inception4e', Inception(528, 256, 160, 320, 32, 128, 128)),
+        ('max4', nn.MaxPool2d(kernel_size=3, stride=2, padding=1)),
+
+        ('inception5a', Inception(832, 256, 160, 320, 32, 128, 128)),
+        ('inception5b', Inception(832, 384, 192, 384, 48, 128, 128))
     ]), OrderedDict([
-        ('fc1', nn.Linear(32 * 4 * 4, 256)),
-        ('*1', None),
-        ('fc2', nn.Linear(256, 10))
+        ('avg1', nn.AdaptiveAvgPool2d((1, 1))),
+        ('dropout1', nn.Dropout(0.4)),
+        ('fc1', nn.Linear(1024, 10))
     ]))
 ]
-candidate_seq_name = {'croissant-like', 'optional1', 'optional2', 'optional3', }
+candidate_seq_name = {'SmallVGG', 'LeNet-5', '2012AlexNet', '2014GoogLeNet', }
 
 candidate_activation_func: List[nn.Module] = [nn.ReLU(), nn.ELU(), nn.LeakyReLU(), nn.SiLU()]
